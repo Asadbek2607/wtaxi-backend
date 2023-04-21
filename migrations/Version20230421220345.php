c6970b9443acaa17619fe75d9b16adb49d21c724
Asadbek Kenjabaev <asadbekadonis@gmail.com>
2023-04-22 03:05:12 +0500

    Payment Class added

---
+++ migrations/Version20230421220345.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+
+final class Version20230421220345 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'create payment table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE `payment` (
+            `id` INT AUTO_INCREMENT NOT NULL, 
+            `method` VARCHAR(255) NOT NULL, 
+            `amount` VARCHAR(255) NOT NULL, 
+            `created_at` DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', 
+            PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE `payment`');
+    }
+}
---
+++ src/Entity/Payment.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\PaymentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaymentRepository::class)]
+#[ApiResource]
+class Payment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $method = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $amount = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMethod(): ?string
+    {
+        return $this->method;
+    }
+
+    public function setMethod(string $method): self
+    {
+        $this->method = $method;
+
+        return $this;
+    }
+
+    public function getAmount(): ?string
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(string $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
