07a97a99b1a4b8a749369afacc875938495dedfe
Asadbek Kenjabaev <asadbekadonis@gmail.com>
2023-04-22 02:10:51 +0500

    migration added for Driver class

--- migrations/Version20230421210518.php
+++ migrations/Version20230421210518.php
@@ -8,21 +8,24 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 
-final class Version20230406203244 extends AbstractMigration
+final class Version20230421210518 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'passenger table created';
+        return 'create driver table';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE TABLE `passenger` (
+        $this->addSql('CREATE TABLE `driver` (
             `id` INT AUTO_INCREMENT NOT NULL, 
             `name` VARCHAR(255) NOT NULL, 
             `email` VARCHAR(255) NOT NULL, 
             `phone_number` VARCHAR(255) NOT NULL, 
+            `license` VARCHAR(255) NOT NULL, 
+            `vehicle` VARCHAR(255) NOT NULL, 
             `password` VARCHAR(255) NOT NULL, 
+            `last_name` VARCHAR(255) NOT NULL, 
             `created_at` DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', 
             `updated_at` DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', 
             `last_login_at` DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', 
@@ -31,6 +34,6 @@ final class Version20230406203244 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql('DROP TABLE `passenger`');
+        $this->addSql('DROP TABLE `driver`');
     }
 }
