26a0049512c444ba683fb953aee86b97bf980190
Asadbek Kenjabaev <asadbekadonis@gmail.com>
2023-04-22 02:20:50 +0500

    Vehicle class added

---
+++ src/Entity/Vehicle.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\VehicleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VehicleRepository::class)]
+#[ApiResource]
+class Vehicle
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $make = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $model = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $color = null;
+
+    #[ORM\Column]
+    private ?int $numberOfSeats = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $licensePlateNumber = null;
+
+    #[ORM\Column]
+    private ?int $year = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $status = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMake(): ?string
+    {
+        return $this->make;
+    }
+
+    public function setMake(string $make): self
+    {
+        $this->make = $make;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getNumberOfSeats(): ?int
+    {
+        return $this->numberOfSeats;
+    }
+
+    public function setNumberOfSeats(int $numberOfSeats): self
+    {
+        $this->numberOfSeats = $numberOfSeats;
+
+        return $this;
+    }
+
+    public function getLicensePlateNumber(): ?string
+    {
+        return $this->licensePlateNumber;
+    }
+
+    public function setLicensePlateNumber(string $licensePlateNumber): self
+    {
+        $this->licensePlateNumber = $licensePlateNumber;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
